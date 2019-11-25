2079f8f813212826ed13ae1389b5cd6aa8143e3d
leavesoul <614354279@qq.com>
2019-11-25 14:45:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView item_tv;
+
+        item_tv = findViewById(R.id.item_tv);
+        item_tv.setText("text1");
+    }
+}
